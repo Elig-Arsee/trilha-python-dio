7551b3b638ce9eeea52c31e85db8587d5efc3965
Elig Arse <arse.eligc@gmail.com>
2023-07-19 16:20:50 -0300

    Ajuste mensagens input e inclusão de descrição com regras

--- 00 - Fundamentos/desafio.py
+++ 00 - Fundamentos/desafio.py
@@ -1,9 +1,32 @@
-menu = """
+# OBJETIVO
+# Criar V1 de sistema bancário com apenas três operações: sacar, depositar e visualizar extrato.
 
-[d] Depositar
-[s] Sacar
-[e] Extrato
-[q] Sair
+# REGRAS DE NEGÓCIO #
+
+# Operação depósito:
+# Deposita somente valores positivos
+# A v1 do projeto trabalha somente com 1 usuário, dessa forma não precisamos nos preocupar em identificar o número da agência e conta bancária.
+# Todos os depósitos devem ser armazenados em uma variável e exibidos na operação de extrato.
+
+# Operação de Saque
+# O sistema deve permitir realizar 3 saques diários com limite máximo de R$ 500,00 por saque.
+# Caso o usuário não tenha saldo em conta, o sistema deve exibir uma mensagem informando que não será possível sacar o dinheiro
+# por falta de saldo.
+# Todos os saques devem ser armazenados em uma variável e exibidos na operação de extrato.
+
+# Operação extrato
+# Essa operação deve listar todos os depósitos e saques realizados na conta.
+# No fim da listagem deve ser exibido o saldo atual da conta.
+# Se o extrato estiver em branco, exibir a mensagem: Não foram realizadas movimentações.
+# Os valores devem ser exibidos utilizando o formato R$ xxx.xx, exemplo: 1500.45 = R$ 1500.45
+
+
+menu = """
+    Boas vindas ao nosso banco digital!       
+Selecione a opção desejada e tecle "enter":
+
+    [1] Depósito         [2] Saque
+    [3] Extrato          [4] Sair
 
 => """
 
@@ -17,17 +40,20 @@ while True:
 
     opcao = input(menu)
 
-    if opcao == "d":
+# Função para realizar a operação de depósito.
+    if opcao == "1":
         valor = float(input("Informe o valor do depósito: "))
 
         if valor > 0:
             saldo += valor
             extrato += f"Depósito: R$ {valor:.2f}\n"
+            print("Insira o valor na boca do caixa.")
 
         else:
             print("Operação falhou! O valor informado é inválido.")
 
-    elif opcao == "s":
+# Função para realizar a operação de saque
+    elif opcao == "2":
         valor = float(input("Informe o valor do saque: "))
 
         excedeu_saldo = valor > saldo
@@ -40,7 +66,7 @@ while True:
             print("Operação falhou! Você não tem saldo suficiente.")
 
         elif excedeu_limite:
-            print("Operação falhou! O valor do saque excede o limite.")
+            print("Operação falhou! O valor do saque excede o limite diário de R$500,00.")
 
         elif excedeu_saques:
             print("Operação falhou! Número máximo de saques excedido.")
@@ -53,13 +79,14 @@ while True:
         else:
             print("Operação falhou! O valor informado é inválido.")
 
-    elif opcao == "e":
+# Função para realizar a operação de extrato.
+    elif opcao == "3":
         print("\n================ EXTRATO ================")
         print("Não foram realizadas movimentações." if not extrato else extrato)
         print(f"\nSaldo: R$ {saldo:.2f}")
         print("==========================================")
 
-    elif opcao == "q":
+    elif opcao == "4":
         break
 
     else:
